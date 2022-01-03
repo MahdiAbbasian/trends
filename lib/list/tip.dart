fdaea7bd288e2de06674e828d6921769930ef678
mahdiabbasian <mam-1371@outlook.com>
2022-01-03 22:10:52 +0330

    add tip of week

---
+++ lib/list/tip.dart
@@ -0,0 +1,34 @@
+class TipOfTheDay {
+  String question;
+  String answer;
+  int num;
+
+  TipOfTheDay(this.question, this.answer, this.num);
+}
+
+List<TipOfTheDay> tipList = [
+  TipOfTheDay(
+      "What Does Claudication Mean?",
+      "Claudication is pain in your thigh or calf that happens when you walk, it can make you lim As it gets worse, the pain occurs when you walk shorter distances. Over time, it can become too painful to even walk.It is usually a symptom of PAD.Types of claudication include feelings of pain, burning feeling, or a tired feeling in the legs. The leg may become pale when raised and red when lowered. There may also be cold feet.Blockages in the leg are most common in the thigh and behind the knee.Tip: Pain in the legs when you are resting is a sign that your blocked arteries have become worse. Speak to your doctor promptly.",
+      0),
+  TipOfTheDay(
+      "What is Restenosis? ",
+      "Restenosis means that   the blocked artery that was opened up with angioplasty or a stent has become narrowed again.  It is a gradual re-narrowing of the artery and is caused by overgrowth of scar tissue.It is usually treated with another stent or angioplasty using a balloon.It occurs often in patients within 6-12 months after a stent is placed.",
+      1),
+  TipOfTheDay(
+      "Smart Stents Detect Narrowing Arteries",
+      "Researchers at the University of British Columbia have figured out that stents could do a lot more than just be a tube inside an artery.  They have created a ‘smart stent’ empowered with sensors that can monitor and provide real-time feedback on blood flow. The feedback can detect re-narrowing of a stented artery.Click here to read more:https://aabme.asme.org/posts/smart-stents-detect-narrowing-arteries",
+      2),
+  TipOfTheDay(
+      "Keep Your Veins Healthy by Eating Lots of Fiber",
+      "Fiber is important in keeping veins healthy.  It is a key nutrient to help keep your digestive system working correctly and keeps pressure from building up in your abdomen. More abdomen pressure means your veins will suffer due to blockage of blood flow to your veins.Reach for foods such as:Popcorn,Oatmeal,Brown Rice,Leafy greens,Broccoli,Avocado,Chia seeds,Lentils,",
+      3),
+  TipOfTheDay(
+      "Why is Artherosclerosis Important?",
+      'Artherosclerosis is also called ‘hardening of the arteries’.  It occurs when fat and cholesterol build up in the walls of the arteries.  These deposits are called plaque and plaque can narrow and even block the arteries. This makes it harder for the oxygen rich blood to flow through them.',
+      4),
+  TipOfTheDay(
+      "What is the Difference Between and Artery and a Vein?",
+      "Arteries are blood vessels that carry oxygen rich blood away from the heart and to the body.Veins are blood vessels that carry blood from the body back into the heart.",
+      5),
+];
