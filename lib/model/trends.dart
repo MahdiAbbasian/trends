6947124376bb6c6655551fdde53ba820e1d60fb3
mahdiabbasian <mam-1371@outlook.com>
2022-01-03 22:11:11 +0330

    config trends model

---
+++ lib/model/trends.dart
@@ -0,0 +1,11 @@
+class Trends {
+  String title;
+  DateTime date;
+  String physicalActivity;
+  String exercise;
+  String medications;
+  String visuals;
+
+  Trends(this.title, this.date, this.physicalActivity, this.exercise,
+      this.medications, this.visuals);
+}
